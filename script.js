56c3a2a2524976009cd368b4f1043aed3304bb6c
Caio Bortolini <95726964+Caio-Bortolini@users.noreply.github.com>
2022-04-18 16:42:36 -0300

    Page rachi
    v1

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnMobile = document.getElementById("btn-mobile");
+
+function toogleMenu() {
+  const nav = document.getElementById('nav');
+
+ nav.classList.toggle('active')
+
+  
+}
+
+btnMobile.addEventListener("click", toogleMenu);
